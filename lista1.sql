23c06e1e4009d0f20558e620a90f55f4b8ae8911
Nicolly Moreira do Carmo <nicollymoreira840@gmail.com>
2023-10-10 12:07:47 -0300

    Ecxercicio 1 concluido!

---
+++ lista1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE nomes (
+    nome VARCHAR(50)
+);
+
+INSERT INTO nomes (nome)
+VALUES
+    ('Roberta'),
+    ('Roberto'),
+    ('Maria Clara'),
+    ('João');
+
+SELECT UPPER(nome) FROM nomes
+
+SELECT nome, LENGTH(nome) AS tamanho FROM nomes;
+
+SELECT
+    CASE
+        WHEN nome LIKE '%Maria%' THEN CONCAT('Sra. ', nome)
+        ELSE CONCAT('Sr. ', nome)
+    END AS nome_formatado
+FROM nomes;
