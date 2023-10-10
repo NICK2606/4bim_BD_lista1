87536f93dafe05f93a65ad891b6cd1c53ad3313c
Nicolly Moreira do Carmo <nicollymoreira840@gmail.com>
2023-10-10 12:12:50 -0300

    Exercicio 2 concluido

--- lista1.sql
+++ lista1.sql
@@ -19,3 +19,22 @@ SELECT
         ELSE CONCAT('Sr. ', nome)
     END AS nome_formatado
 FROM nomes;
+
+
+CREATE TABLE produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos (produto, preco, quantidade)
+VALUES
+    ('Produto A', 10.50, 5),
+    ('Produto B', 20.75, 3),
+    ('Produto C', 15.00, 0);
+
+SELECT produto, ROUND(preco, 2) AS preco_arredondado FROM produtos;
+
+SELECT produto, ABS(quantidade) AS quantidade_absoluta FROM produtos;
+
+SELECT AVG(preco) AS media_precos FROM produtos;
